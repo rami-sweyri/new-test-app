1ed86f1189f0851847ef6e517d545fb50c780dba
Rami Sweyri <rami@larsa.org>
2020-08-16 15:18:06 +0300

    v.06

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
+const purgeCSS = require("@fullhuman/postcss-purgecss");
+
 module.exports = {
   plugins: [
-    // ...
     require("tailwindcss"),
-    require("autoprefixer"),
-    // ...
+    purgeCSS({
+      content: ["./src/**/*.js"],
+      css: ["./src/**/*.css"],
+    }),
   ],
 };
